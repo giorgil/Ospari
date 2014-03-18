3f513d0b8ade252a2891c9514c0096dac220bad8
Fon-Pah <fmn@28h.eu>
2014-03-18 14:29:45 +0100

    Added required vendors,
    implemented the composer method of installing application dependencies

--- core/vendor/NZ/html/templates/basic/hidden.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$field = $this->data[NZ\BootstrapFilterForm::FIELD_FIELD];
-$values = $this->data[NZ\BootstrapFilterForm::FIELD_VALUES];
-
-?>
-
-<input type="hidden" name="<?= $field ?>" value="<?= $values; ?>">
